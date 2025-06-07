86331d52a68f92849172be19ca2d5c054e2f1acc
SteveBattista <stephen.battista@gmail.com>
2025-06-07 16:24:35 -0400

    Refactor install_script.sh to streamline package installation and remove unnecessary dependencies

--- bin/install_script.sh
+++ bin/install_script.sh
@@ -2,13 +2,14 @@
 # This script installs the necessary dependencies for the project.
 # It is intended to be run on a fresh Ubuntu 22.04 installation.
 #install items
-sudo apt install curl pv jq ssdeep exiftool vlc fish gstreamer0.10-ffmpeg gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad gstreamer0.10-bad-multiverse -y
+apt install curl pv jq ssdeep exiftool vlc fish -y
+# update and upgrade
+apt update && apt upgrade -y
 # remove items 
-sudo apt remove rhythmbox libreoffice cups apt remove gnome-games remove gnome-chess -y
-#update and upgrade
-sudo apt update && apt upgrade -y
+apt remove cups thunderbird -y
 #clean up
 sudo apt autoremove -y
+git 
 echo 'export PATH="/install/RAMSAFE/bin:$PATH"' >> ~/.bashrc
 line="@reboot /install/RAMSAFE/bin/on_boot.sh"
  echo "$line"  | crontab -u root -

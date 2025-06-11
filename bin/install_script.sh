6c6e89d30cb0dacbf70211474d79980ba43ec67c
SteveBattista <stephen.battista@gmail.com>
2025-06-11 18:27:11 -0400

    Enhance output messages in install_script.sh for better clarity during installation steps

--- bin/install_script.sh
+++ bin/install_script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # This script installs the necessary dependencies for the project.
 # It is intended to be run on a fresh Ubuntu 22.04 installation.
-echo Installing items
+echo ********** Installing items **********
 apt install curl pv jq ssdeep exiftool vlc fish -y
-echo Update and upgrade
+echo ********** Update and upgrade **********
 apt update && apt upgrade -y
-echo removing items 
+echo ********** Removing items  **********
 apt remove cups thunderbird -y
 apt autoremove --purge -y "?name(libreoffice)"
 apt autoremove --purge -y "?name(ubuntu-desktop-bootstrap)"
 apt autoremove --purge -y "?name(thunderbird)"
 apt autoremove --purge -y "?name(shotwell)"
-echo Clean up
+echo ********** Clean up **********
 apt autoremove -y
-echo Add tools to PATH
+echo ********** Add tools to PATH **********
 echo 'export PATH="/install/RAMSAFE/bin:$PATH"' >> ~/.bashrc
 
 

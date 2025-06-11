01f678925bf251ca1fa224bf641c807f0eaa7223
SteveBattista <stephen.battista@gmail.com>
2025-06-11 18:25:00 -0400

    Refactor install_script.sh for improved readability and organization of installation steps

--- bin/install_script.sh
+++ bin/install_script.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 # This script installs the necessary dependencies for the project.
 # It is intended to be run on a fresh Ubuntu 22.04 installation.
-#install items
+echo Installing items
 apt install curl pv jq ssdeep exiftool vlc fish -y
-# update and upgrade
+echo Update and upgrade
 apt update && apt upgrade -y
-# remove items 
+echo removing items 
 apt remove cups thunderbird -y
-#clean up
-apt autoremove -y
-# move desktop graphics item 
-echo 'export PATH="/install/RAMSAFE/bin:$PATH"' >> ~/.bashrc
-#apt autoremove --purge snapd
 apt autoremove --purge -y "?name(libreoffice)"
+apt autoremove --purge -y "?name(ubuntu-desktop-bootstrap)"
 apt autoremove --purge -y "?name(thunderbird)"
 apt autoremove --purge -y "?name(shotwell)"
+echo Clean up
+apt autoremove -y
+echo Add tools to PATH
+echo 'export PATH="/install/RAMSAFE/bin:$PATH"' >> ~/.bashrc
+
+
+
 
 
 

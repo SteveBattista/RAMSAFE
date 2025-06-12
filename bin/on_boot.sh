bd5a9b8f77384013228366d840bea0e5d241f9df
SteveBattista <stephen.battista@gmail.com>
2025-06-12 18:39:20 -0400

    Update on_boot.sh to remove auto-start of ubuntu-desktop-bootstrap.desktop and stop gnome Ubiquity installer

--- bin/on_boot.sh
+++ bin/on_boot.sh
@@ -3,7 +3,8 @@
 gsettings set org.gnome.desktop.background picture-uri 'file:///install/RAMSAFE/ramsafe_wallpaper.png'
 #set icons on left
 gsettings set org.gnome.shell favorite-apps "['firefox_firefox.desktop', 'org.gnome.eog.desktop', 'vlc.desktop', 'org.gnome.TextEditor.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop']"
-#Stop auto start of gnome Ubiquity installer
-systemctl stop ubiquity
-#Remove auto start of gnome Ubiquity installer
-systemctl disable ubiquity
+# remove an icon from the desktop
+gsettings set org.gnome.shell.extensions.desktop-icons ubuntu-desktop-bootstrap_ubuntu-desktop-bootstrap.desktop false
+# don't auto start ubuntu-desktop-bootstrap.desktop when starting the GUI
+
+
